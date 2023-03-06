7df54b77c60d21262db9a886b22e911d822bc4f4
Vladislav <91069241+Asphodell@users.noreply.github.com>
2023-03-06 22:14:55 +0300

    task B

---
+++ class.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+
+	typedef std::multimap<int, std::string> strMap;
+	strMap m;
+
+	/*std::fstream f;
+	f.open("myFile.txt");*/
+	//std::fstream file("myFile.txt");
+
+	/*while (file) {
+		std::string s;
+		std::getline(file, s);
+		if (s[0] == '9') {
+			m.insert(std::make_pair(s[0] - 48, s.substr(2)));
+		}
+		else if(s[0] == '1' && (s[1] =='0' || s[1] == '1')) {
+			char s1;
+			s1 = s[0] + s[1];
+			m.insert(std::make_pair(s1 - 87, s.substr(3)));
+		}
+	}*/
+
+	for (int i = 0; i < 100000; ++i){
+		std::string s;
+		std::getline(std::cin, s);
+		if (s[0] == '9') {
+			int num = stoi(s.substr(0, 1));
+			m.insert(std::make_pair(num, s.substr(2)));
+		}
+		else if (s[0] == '1' && (s[1] == '0' || s[1] == '1')) {
+			int num = stoi(s.substr(0, 2));
+			m.insert(std::make_pair(num, s.substr(3)));
+		}
+		
+	}
+
+	for (auto& i : m) {
+		std::cout << i.first << " " << i.second << std::endl;
+	}
+
+	return 0;
+}
